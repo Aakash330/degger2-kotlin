5d8e4211565915710816f44d68dbecfc82389edf
Aakash Kumar <aakash.kumar@trignodev.com>
2022-07-12 08:13:11 +0530

    use of @binds

--- app/src/main/java/com/example/degger2_kotlin/module/UserRepositoryModule.kt
+++ app/src/main/java/com/example/degger2_kotlin/module/UserRepositoryModule.kt
@@ -1,16 +1,20 @@
 package com.example.degger2_kotlin.module
 
 import com.example.degger2_kotlin.Firebase_Repository
+import com.example.degger2_kotlin.Sql_Repository
 import com.example.degger2_kotlin.User_Repository
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 
 @Module
-class UserRepositoryModule {
+abstract class UserRepositoryModule {
 
-    @Provides
-    fun getFirebaseRepository(): User_Repository
+   /* @Provides
+    fun getSqlRepository(sqlRepository: Sql_Repository): User_Repository
     {
-        return Firebase_Repository()
-    }
+        return sqlRepository
+    }*/
+    @Binds
+    abstract fun getSqlRepsitory(sqlRepository: Sql_Repository):User_Repository
 }
